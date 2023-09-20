399025e2319fb3db7762da40824c763008368ec9
eduroxx <richa.anchan27@gmail.com>
2023-09-20 11:52:36 +0530

    return 0 removed

---
+++ 1_Print_Output.cpp
@@ -0,0 +1,12 @@
+// Q1 - Write a program to print "Welcome to Talent Battle - Placement Cracker Sheet" on output screen.//
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+string a = "Welcome to Talent Battle - Placement Cracker Sheet";
+cout<<a;
+
+}
