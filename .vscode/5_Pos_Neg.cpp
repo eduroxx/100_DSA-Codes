b6db49b785953303593d2108992d7013dc64f721
eduroxx <richa.anchan27@gmail.com>
2023-09-20 18:41:00 +0530

    question 5

---
+++ .vscode/5_Pos_Neg.cpp
@@ -0,0 +1,16 @@
+//Q5 -Write a program to identify of the a number is positive or negative//
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n ;
+    cin>>n;
+    if(n>0)
+    cout  << n << "is Positive" ;
+    else
+    cout << n << "is Negative" ;
+}
+
+
