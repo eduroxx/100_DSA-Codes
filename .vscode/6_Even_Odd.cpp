753a551c88d599777b82c34d6c3b26e8584643a2
eduroxx <richa.anchan27@gmail.com>
2023-09-21 14:25:46 +0530

    question 6

---
+++ .vscode/6_Even_Odd.cpp
@@ -0,0 +1,16 @@
+// Q6 - Write a program to identify if the number is even or odd//
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+int n;
+cin>>n;
+if(n%2==0)
+cout<< n <<"is even"<<endl;
+else
+cout<< n <<" is odd"<<endl;
+}
+
